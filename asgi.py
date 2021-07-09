29787615cf6202c8662fef2dea61342b1b387dbb
alexsander.vieira <alexsandervieirajunior@hotmail.com>
2021-07-09 18:54:21 -0300

    Leo Project

--- asgi.py
+++ asgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
-application = get_asgi_application()
+application = Cling(get_wsgi_application())
